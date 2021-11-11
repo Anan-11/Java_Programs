108d98b2f5b18661ba8ca6a004fab7ed885e1832
Anan-11 <89539818+Anan-11@users.noreply.github.com>
2021-11-12 01:08:34 +0530

    Create Area_Volume_of_Box.java

---
+++ Area_Volume_of_Box.java
@@ -0,0 +1,30 @@
+// program to print area and volume of a box(cuboid)
+class box
+{
+    public int length;
+    public int height;
+    public int breadth;
+}
+class box3d extends box {
+    public box3d(int length, int breadth, int height) {
+        this.length = length;
+        this.breadth = breadth;
+        this.height = height;
+
+    }
+
+    public int area() {
+        int n = 2 * (length * breadth + breadth * height + height * length);
+        return n;
+    }
+
+    public int volume() {
+        return length * breadth * height;
+    }
+
+    public static void main(String[] args) {
+        box3d obj=new box3d(10,20,30);
+        System.out.println(obj.area());
+        System.out.println(obj.volume());
+    }
+}
