710ed5e9eeac2866f35e44e627a1730dc125103e
Anan-11 <89539818+Anan-11@users.noreply.github.com>
2021-11-12 01:33:08 +0530

    Create Constructor_Overloading_.java

---
+++ Constructor_Overloading_.java
@@ -0,0 +1,27 @@
+  //  Example of Constructor Overloading
+ //Java program to overload constructors
+ 
+ class Student5{
+     int id;
+     String name;
+     int age;
+     //creating two arg constructor
+     Student5(int i,String n){
+         id = i;
+         name = n;
+     }
+     //creating three arg constructor
+     Student5(int i,String n,int a){
+         id = i;
+         name = n;
+         age=a;
+     }
+     void display(){System.out.println(id+" "+name+" "+age);}
+
+     public static void main(String args[]){
+         Student5 s1 = new Student5(111,"ewdewd",12);
+         Student5 s2 = new Student5(222,"Aryan");
+         s1.display();
+         s2.display();
+     }
+ }
