65fa0ff120c54daa1a04d7ce8fb72652244ae6c1
Anan-11 <89539818+Anan-11@users.noreply.github.com>
2021-11-12 01:54:24 +0530

    Create Super_keyword_1.java

---
+++ Super_keyword_1.java
@@ -0,0 +1,32 @@
+// use of super keyword to perform method overriding in java.
+
+//super keyword is used to call the method or the constructor of the immediate parent class.
+
+import java.util.*;
+import java.io.*;
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me(); 
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
