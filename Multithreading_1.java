b4b04c0414d64cb22fd3e1df4dcc2b4b4946020c
Anan-11 <89539818+Anan-11@users.noreply.github.com>
2021-11-21 14:20:16 +0530

    Create Multithreading_1.java
    Topic- Multithreading
    Q) store colours in the form of an array
    ex: String colours[]={"white","blue","black","green","red","yellow"};
    display all colours repeatedly by generating colour index from Random class. If the random colour index matches to red stop display.
    Note: perform this task by implementing Runnabe interface

---
+++ Multithreading_1.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+class Thread2 implements Runnable{
+    public void run()
+    {
+        String []colors={"white","blue","black","green","red","yellow"};
+        Random random=new Random(5);
+        int flag=0;
+        while(flag!=1) {
+            int i1 = random.nextInt();
+            if (i1 >= 0 && i1 <= 5) {
+                if (!colors[i1].equals("red")) {
+                    System.out.println(colors[i1]);
+                } else {
+                    flag++;
+                }
+
+            }
+        }
+    }
+}
+
+class Tmain1{
+    public static void main(String[] args) {
+        Thread obj=new Thread(new Thread2());
+        obj.start();
+
+
+
+    }
+}
